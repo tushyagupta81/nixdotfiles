c328e94baa5b97e9089c450339540d128bc68c3c
Tushya Gupta <tushyagupta81@gmail.com>
2024-09-10 16:03:37 +0530

    missing steps in install script

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ curl --proto '=https' --tlsv1.2 -sSf \
   -L https://install.determinate.systems/nix \
   | sh -s -- install
 
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
 $HOME/nixdotfiles/scripts/switch-nix
+
+echo "Changing default shell to fish"
+echo "$(which fish)" | sudo tee -a /etc/shells >/dev/null
+# Set the Homebrew fish as default shell
+chsh -s "$(which fish)"
